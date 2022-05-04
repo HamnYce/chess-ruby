97f390e313a207575d078a4da9ed227e2e99ea91
HamnYce <hamadboa@gmail.com>
2022-05-04 04:05:53 +0300

    create check method (with helper methods). possible_moves will stop once theres a piece. remove delete_blocked_moves. add methods for convenience (current_king_pos, other_king_pos, update_curr_king_pos, print_table)

--- lib/board.rb
+++ lib/board.rb
@@ -8,12 +8,15 @@ require_relative 'movement'
 class Board
   include Piece
   include MovementAlgs
+  include GroupedMovementProcs
 
+  # TODO: convert implementation of @table to hash
   def initialize
     @table = STARTING_BOARD
     @wking_pos = [0, 4]
     @bking_pos = [7, 4]
     @curr_player_white = true
+    print_table
   end
 
   def move(init_pos, fin_pos)
@@ -23,35 +26,34 @@ class Board
 
     dir = direction(init_pos, fin_pos)
 
-    return 'piece can\'t move like that' unless piece.include_mov?(dir)
+    return 'piece can\'t move like that' unless piece.include_dir?(dir)
 
-    p_moves = piece_moves(init_pos, dir)
+    # TODO: seperate out the pawn movement from capturing.
+    #   you can add all pawn movement and just revert when an illegal move is
+    #   done. (moving diagonally or capturing straight)
+    #   since movement and capturing are considered the same action all you have
+    #   to do is add if statement
+    p_moves = piece_moves(piece, init_pos, dir)
 
-    f_moves = delete_blocked_moves(p_moves) unless piece.is_a? Knight
-
-    return 'piece is blocked' unless f_moves.include?(fin_pos)
-
+    return 'piece is blocked' unless p_moves.include?(fin_pos)
     return 'cannot capture own piece' if same_team?(piece, get_piece(fin_pos))
+    return 'pawn cannot move like that' if illegal_pawn_move?(fin_pos, dir)
 
     simulate(init_pos, fin_pos)
 
-    kings = @curr_player_white ? [@wking_pos, @bking_pos] : [@bking_pos, @wking_pos]
+    return revert_simulation if can_be_attacked?(current_king_pos)
+    return 'Checkmate!' if checkmate?(other_king_pos)
 
-    if check?(kings[0])
-      revert_simulation(@attacker, @defender)
-      return 'illegal move, own king in check'
-    end
+    # Updating king position if king is the one that moved
+    update_curr_king_pos(fin_pos) if @attacker[1].king?
 
-    if checkmate?(kings[1])
-      winner = @curr_player_white ? 'white' : 'black'
-      return "#{winner} is the winner"
-    end
-    # if own king is in check revert simulation
+    system('clear')
+    print_table
 
-    @curr_player_white = !@curr_player_white
+    flip_current_player
   end
 
-  def piece_moves(init_pos, dir)
+  def piece_moves(piece, init_pos, dir)
     case piece
     when Knight
       knight_possible_moves(init_pos, dir)
@@ -62,55 +64,25 @@ class Board
     end
   end
 
-  # creates a new board with the movement passed having already occured
-  # TODO: search for check state in simulated board
-  # if no checks against player king in simulated board
-  # check for check against opponent king
-  # NOTE: instead of simulating the whole board -> simulate piece movements
   def simulate(init_pos, fin_pos)
     @attacker = [init_pos, get_piece(init_pos)]
     @defender = [fin_pos, get_piece(fin_pos)]
 
-    if @attacker.is_a? King
-      @curr_player_white ? @wking_pos = fin_pos : @bking_pos = fin_pos
-    end
-
     move_and_overwrite(init_pos, fin_pos)
   end
 
-  def revert_simulation(attacker, defender)
-    row, col = attacker[0]
-    @table[row][col] = attacker[1]
+  def revert_simulation
+    row, col = @attacker[0]
+    @table[row][col] = @attacker[1]
 
-    if attacker.is_a? King
-      @curr_player_white ? @wking_pos = attacker[0] : @bking_pos = attacker[0]
-    end
+    row, col = @defender[0]
+    @table[row][col] = @defender[1]
 
-    row, col = defender[1]
-    @table[row][col] = defender
-  end
-
-  # loops through p_moves, if piece is present at pos (piece_exists)
-  # deletes everything AFTER (not including) that pos
-  # returns f_moves
-  def delete_blocked_moves(p_moves)
-    blockage_index = first_blockage(p_moves)
-    (p_moves.length - blockage_index - 1).times { p_moves.pop }
-    p_moves
-  end
-
-  # DOC: returns index of first non-nil element in array using @table
-  #       or moves length if none is found
-  def first_blockage(moves)
-    (0..(moves.length - 1)).each do |i|
-      pos = moves[i]
-      return i unless @table[pos[0]][pos[1]].nil?
-    end
-    moves.length
+    'illegal move, own king is in check'
   end
 
   # copies piece reference to fin_pos and removes reference from init_pos
-  def move_and_overwrite(board, init_pos, fin_pos)
+  def move_and_overwrite(init_pos, fin_pos)
     attacker = @table[init_pos[0]][init_pos[1]]
 
     @table[fin_pos[0]][fin_pos[1]] = attacker
@@ -118,42 +90,88 @@ class Board
   end
 
   # use @current_player_white to figure out which king
-  def checkmate?
+  def checkmate?(king_pos)
     # if piece can't move out of the way
     # cant block
     # piece attacker isn't capturable
   end
 
-  # check if king can move anywhere without being in check in resulting move
-  # NOTE: check if empty spaces around the king can be attacked
-  # NOTE: use position instead of simulating
+  # check all surrounding panels from the other_king(use king movement)
   # TODO: Implement can_be_attacked? first then just check the spaces around the king
-  def king_can_move?(king_pos); end
+  #   use this for checkmate on other_king_pos
+  #   check space around king to see if he has a possible move, for checkmate
+  def king_can_move?(king_pos)
+    all_moves = KINGPATHS.map { |dir| possible_move(king_pos, dir) }
 
-  # can the distance between attacker and king be blocked by another piece?
+    all_moves.compact.each { |pos| return true unless piece_exists?(pos) }
+  end
+
   # assuming it's not a knight
-  # check if same team piece can move inbetween the 2 positions
-  def attack_blockable?(attacker_pos, king_pos); end
+  # check if same team piece can move inbetween the 2 positions (use class to
+  # pick the direction for possible moves)
+  def attack_blockable?(attacker_pos, king_pos, attacker_class); end
 
   # NOTE: use to check around king in all directions for check? & checkmate?
   # diagonal paths for bishop || queen || pawn (opposite team)
   # straight paths for rook || queen (opposite team)
-  # check all movemment directions
-  # FIXME: doesn't account for knight or Pawn
+  # check all movement directions
   # DOC: check if target pos can be attacked by opposite team
-  def can_be_attacked?(def_pos, att_pos, team)
-    def_dir = direction(def_pos, att_pos)
-    p_moves = possible_moves(direction)
-    piece_pos = p_moves.each do |pos|
-        break pos unless @table[pos[0]][pos[1]].nil?
-      end
-    piece = @table[piece_pos[0]][piece_pos[1]]
-    def_dir = %i[UP DOWN RIGHT LEFT].include?(def_dir) ? :STRAIGHT : :DIAG
-    if def_dir == STRAIGHT && (piece.is_a?(Rook) || piece.is_a?(Queen))
-      return true
-    elsif piece.is_a?(Bishop) || piece.is_a?(Knight) || piece.is_a?(Queen)
-      return true
+  # this is check
+  def can_be_attacked?(defender_pos)
+    linear_check(defender_pos, STRAIGHTPATHS, Rook) ||
+      linear_check(defender_pos, DIAGPATHS, Bishop) ||
+      knight_check(defender_pos) ||
+      pawn_check(defender_pos)
+  end
+
+  def linear_check(defender_pos, directions, class_name)
+    # for each direction we get the last possible movement using
+    # possible_moves & last
+    # this gives us an array with positions that should either be an empty
+    # space next to a wall or occupied by a piece
+    all_moves = directions.map { |dir| possible_moves(defender_pos, dir).last }
+
+    all_moves.compact.each do |attacker_pos|
+      # skip if no piece at pos
+      next unless piece_exists?(attacker_pos)
+
+      piece = get_piece(attacker_pos)
+
+      # if piece is opposite team and appropriate piece then defender_piece is
+      # under attack
+      case piece when class_name || Queen then return true end if other_team?(piece)
     end
+
+    false
+  end
+
+  def knight_check(defender_pos)
+    all_moves = KNIGHTPATHS.map { |dir| possible_move(defender_pos, dir).last }
+
+    all_moves.compact.each do |attacker_pos|
+      next unless piece_exists?(attacker_pos)
+
+      piece = get_piece(attacker_pos)
+
+      return true if other_team?(piece) && piece.knight?
+    end
+
+    false
+  end
+
+  def pawn_check(defender_pos)
+    directions = @curr_player_white ? WHITEPAWNPATHS : BLACKPAWNPATHS
+
+    all_moves = directions.map { |dir| possible_move(defender_pos, dir) }
+
+    all_moves.compact.each do |attacker_pos|
+      next unless piece_exists?(attacker_pos)
+
+      piece = get_piece(attacker_pos)
+
+      return true if other_team?(piece) && piece.pawn?
+    end
+
     false
   end
 
@@ -174,10 +192,9 @@ class Board
     direction.to_sym
   end
 
-  # DOC: checks if piece exists at pos
-  def piece_exists?(pos)
-    !@table[pos[0]][pos[1]].nil?
-  end
+  # FIXME: piece_exists? && diff_team? (maybe create this with nil being false)
+  #   && diagpath || (UP && !piece_exists?)
+  def illegal_pawn_move?(fin_pos, dir); end
 
   # returns true if a_piece@team_white != b_piece@team_white
   def same_team?(a_piece, b_piece)
@@ -189,4 +206,60 @@ class Board
   def get_piece(pos)
     @table[pos[0]][pos[1]]
   end
+
+  def flip_current_player
+    @curr_player_white = !@curr_player_white
+  end
+
+  def current_king_pos
+    @curr_player_white ? @wking_pos  : @bking_pos
+  end
+
+  def other_king_pos
+    @curr_player_white ? @bking_pos : @wking_pos
+  end
+
+  def update_curr_king_pos(new_pos)
+    if @curr_player_white
+      @wking_pos = new_pos
+    else
+      @bking_pos = new_pos
+    end
+  end
+
+  def other_team?(piece)
+    piece.team_white != @curr_player_white
+  end
+
+  def print_table
+    puts '   --- --- --- --- --- --- --- ---'
+    @table.each_with_index do |row, i|
+      print "#{8 - i} |"
+
+      row.each do |panel|
+        print " #{panel.nil? ? ' ' : panel} |"
+      end
+
+      puts "\n   --- --- --- --- --- --- --- ---"
+    end
+    puts '    a   b   c   d   e   f   g   h '
+  end
+
+  # example user input:
+  # init_a,init_b int,letter
+  # fin_a,fin_b int, letter
+  def translate
+  end
 end
+
+b = Board.new
+
+gets
+
+b.move([6, 0], [5, 1])
+
+gets
+
+b.move([7, 1], [5, 2])
+
+gets
