b7947ef41e3f9a23c52675838588a0db28d9aa37
HamnYce <hamadboa@gmail.com>
2022-05-06 01:10:06 +0300

    implement king_can_move? (untested) add checkmate? (untested)

--- lib/board.rb
+++ lib/board.rb
@@ -97,8 +97,6 @@ class Board
       return response('self check')
     end
 
-    return 'Checkmate!' if checkmate?(other_king_pos)
-
     update_curr_king_pos(fin_pos) if @attacker[1].king?
 
     # Phase 5 conditions
@@ -119,15 +117,23 @@ class Board
   #   can_be_attacked? on spaces between king and attacker (can attack be
   #   blocked)
   #   can_be_attacked? on attacking piece (can attacker be captured)
-  def checkmate?(king_pos)
-    # if piece can't move out of the way
-    # cant block
-    # piece attacker isn't capturable
+  def checkmate?
+    !king_can_move? &&
+      !can_be_attacked?(attacker_pos) &&
+      can_be_attacked?(king_pos)
   end
 
+  def king_can_move?
+    king = get_piece(other_king_pos)
+    all_moves = KINGPATHS.map { |dir| possible_move(other_king_pos, dir).last }
+    all_moves.compact.each do |pos|
 
-  def king_can_move?(king_pos)
-    all_moves = KINGPATHS.map { |dir| possible_move(king_pos, dir) }
+      # if position around king is not under attack AND there exists an
+      # opposite colored piece OR no piece at all then king can move
+
+      return true if !can_be_attacked?(pos) && !same_team?(king, get_piece(pos))
+    end
+    false
   end
 
   # assuming it's not a knight
@@ -137,8 +143,8 @@ class Board
 
   # DOC: check if target pos can be attacked by opposite team
   def can_be_attacked?(defender_pos)
-    linear_check(defender_pos, STRAIGHTPATHS, Rook) ||
-      linear_check(defender_pos, DIAGPATHS, Bishop) ||
+    linear_check(defender_pos, STRAIGHTPATHS) ||
+      linear_check(defender_pos, DIAGPATHS) ||
       knight_check(defender_pos) ||
       pawn_check(defender_pos)
   end
