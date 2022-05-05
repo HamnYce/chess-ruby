2b7fa1000a3136b35de5c5e8711ae1b0bed4681a
HamnYce <hamadboa@gmail.com>
2022-05-06 01:15:30 +0300

    create serializer.rb

---
+++ lib/serializer.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+
+require 'json'
+
+# Module to serialise the board
+module Serializer
+  def save_game; end
+  # TODO: create main menu screen
+  def load_game; end
+end
