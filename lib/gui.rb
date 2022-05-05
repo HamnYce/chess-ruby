6107f714aca6d5db6389e777fb29b1605d0e621c
HamnYce <hamadboa@gmail.com>
2022-05-06 01:14:24 +0300

    create gui.rb

---
+++ lib/gui.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Gui
+  def print_table
+    puts '   --- --- --- --- --- --- --- ---'
+    @table.each_with_index do |row, i|
+      print "#{8 - i} |"
+
+      row.each do |panel|
+        print " #{panel.nil? ? ' ' : panel} |"
+      end
+
+      puts "\n   --- --- --- --- --- --- --- ---"
+    end
+    puts '    a   b   c   d   e   f   g   h '
+    puts " Current player: #{@curr_player_white ? 'White' : 'Black'}"
+  end
+
+  def response(code)
+    { 'wrong team' => "It is currently #{@curr_player_white ? 'White' : 'Black'}'s turn",
+      'no piece' => 'sorry there is no piece at given starting position',
+      'no dir' => 'sorry piece can\'t move in that direction',
+      'not legal' => 'not legal move',
+      'friendly fire' => 'cannot capture own piece',
+      'rogue pawn' => 'pawn cannot move like that',
+      'self check' => 'your own king is in check!'}[code]
+  end
+end
