d35d85bbad7a5c7f2b775794f53b4e550cbb0ea8
HamnYce <hamadboa@gmail.com>
2022-05-06 09:05:42 +0300

    move check style methods to checker.rb

--- lib/board.rb
+++ lib/board.rb
@@ -111,43 +111,6 @@ class Board
 
     'successful move'
   end
-
-  # NOTE: can_be_attacked? on the spaces around the king (can king move away?)
-  #   on other_king_pos.
-  #   can_be_attacked? on spaces between king and attacker (can attack be
-  #   blocked)
-  #   can_be_attacked? on attacking piece (can attacker be captured)
-  def checkmate?
-    !king_can_move? &&
-      !can_be_attacked?(attacker_pos) &&
-      can_be_attacked?(king_pos)
-  end
-
-  def king_can_move?
-    king = get_piece(other_king_pos)
-    all_moves = KINGPATHS.map { |dir| possible_move(other_king_pos, dir).last }
-    all_moves.compact.each do |pos|
-
-      # if position around king is not under attack AND there exists an
-      # opposite colored piece OR no piece at all then king can move
-
-      return true if !can_be_attacked?(pos) && !same_team?(king, get_piece(pos))
-    end
-    false
-  end
-
-  # assuming it's not a knight
-  # check if same team piece can move inbetween the 2 positions (use class to
-  # pick the direction for possible moves)
-  def attack_blockable?(attacker_pos, king_pos, attacker_class); end
-
-  # DOC: check if target pos can be attacked by opposite team
-  def can_be_attacked?(defender_pos)
-    linear_check(defender_pos, STRAIGHTPATHS) ||
-      linear_check(defender_pos, DIAGPATHS) ||
-      knight_check(defender_pos) ||
-      pawn_check(defender_pos)
-  end
 end
 
 b = Board.new
