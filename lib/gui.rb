47c7d07150e6cda4cf6e9dbbca880041e07a9cad
HamnYce <hamadboa@gmail.com>
2022-05-20 08:42:43 +0300

    add piece icons add upgrade screen gui

--- lib/gui.rb
+++ lib/gui.rb
@@ -1,9 +1,25 @@
 # frozen_string_literal: true
 
+
 module Gui
+  WKING_ICON = "\u265A"
+  WQUEEN_ICON = "\u265B"
+  WROOK_ICON = "\u265C"
+  WBISHOP_ICON = "\u265D"
+  WKNIGHT_ICON = "\u265E"
+  WPAWN_ICON = "\u265F"
+
+  BKING_ICON = "\u2654"
+  BQUEEN_ICON = "\u2655"
+  BROOK_ICON = "\u2656"
+  BBISHOP_ICON = "\u2657"
+  BKNIGHT_ICON = "\u2658"
+  BPAWN_ICON = "\u2659"
   #   e (exit), s (save), se (save & exit), l (load), h (help)
   #   create module for saving
   def print_table
+    system('clear')
+
     puts '   --- --- --- --- --- --- --- ---'
     @table.each_with_index do |row, i|
       print "#{8 - i} |"
@@ -26,5 +42,20 @@ module Gui
       'friendly fire' => 'cannot capture own piece',
       'rogue pawn' => 'pawn cannot move like that',
       'self check' => 'your own king is in check!'}[code]
+  end
+
+  def print_upgrade_screen(team)
+    system('clear')
+
+    queen = team ? WQUEEN_ICON : BQUEEN_ICON
+    rook = team ? WROOK_ICON : BROOK_ICON
+    bishop = team ? WBISHOP_ICON : BBISHOP_ICON
+    knight = team ? WKNIGHT_ICON : BKNIGHT_ICON
+
+    puts "Pawn upgrade in progress:\nInput:\n"
+    puts "1 - #{queen} for Queen"
+    puts "2 - #{rook} for Rook"
+    puts "3 - #{bishop} for Bishop"
+    puts "4 - #{knight} for Knight"
   end
 end
