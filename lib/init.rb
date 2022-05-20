890ac66d8b7897ca494bdfb6932211b0333b8468
HamnYce <hamadboa@gmail.com>
2022-05-20 08:44:18 +0300

    bugfixes to self.normal & self.checkmate_in_one remove checkmate_in_one_debug refactor

--- lib/init.rb
+++ lib/init.rb
@@ -13,41 +13,39 @@ STARTING_BOARD = Array.new(8) { Array.new(8, nil) }
 module StartState
   def self.normal
     # Black's side
-    STARTING_BOARD[0][0] = Rook.new(false)
-    STARTING_BOARD[0][1] = Knight.new(false)
-    STARTING_BOARD[0][2] = Bishop.new(false)
-    STARTING_BOARD[0][3] = Queen.new(false)
-    STARTING_BOARD[0][4] = King.new(false)
-    STARTING_BOARD[0][5] = Bishop.new(false)
-    STARTING_BOARD[0][6] = Knight.new(false)
-    STARTING_BOARD[0][7] = Rook.new(false)
+    STARTINGBOARD[0][0] = Rook.new(false)
+    STARTINGBOARD[0][1] = Knight.new(false)
+    STARTINGBOARD[0][2] = Bishop.new(false)
+    STARTINGBOARD[0][3] = Queen.new(false)
+    STARTINGBOARD[0][4] = King.new(false)
+    STARTBLACKKING << 0 << 4
+    STARTINGBOARD[0][5] = Bishop.new(false)
+    STARTINGBOARD[0][6] = Knight.new(false)
+    STARTINGBOARD[0][7] = Rook.new(false)
     # initialise black pawns
-    (0..7).each { |i| STARTING_BOARD[1][i] = Pawn.new(false) }
+    (0..7).each { |i| STARTINGBOARD[1][i] = Pawn.new(false) }
 
     # White's side
-    STARTING_BOARD[7][0] = Rook.new(true)
-    STARTING_BOARD[7][1] = Knight.new(true)
-    STARTING_BOARD[7][2] = Bishop.new(true)
-    STARTING_BOARD[7][3] = Queen.new(true)
-    STARTING_BOARD[7][4] = King.new(true)
-    STARTING_BOARD[7][5] = Bishop.new(true)
-    STARTING_BOARD[7][6] = Knight.new(true)
-    STARTING_BOARD[7][7] = Rook.new(true)
+    STARTINGBOARD[7][0] = Rook.new(true)
+    STARTINGBOARD[7][1] = Knight.new(true)
+    STARTINGBOARD[7][2] = Bishop.new(true)
+    STARTINGBOARD[7][3] = Queen.new(true)
+    STARTINGBOARD[7][4] = King.new(true)
+    STARTWHITEKING << 7 << 4
+    STARTINGBOARD[7][5] = Bishop.new(true)
+    STARTINGBOARD[7][6] = Knight.new(true)
+    STARTINGBOARD[7][7] = Rook.new(true)
     # initialise white pawns
-    (0..7).each { |i| STARTING_BOARD[6][i] = Pawn.new(true) }
+    (0..7).each { |i| STARTINGBOARD[6][i] = Pawn.new(true) }
   end
 
   def self.checkmate_in_one
-    STARTING_BOARD[0][0] = King.new(false)
-    STARTING_BOARD[2][0] = King.new(true)
-    STARTING_BOARD[2][3] = Queen.new(true)
-  end
-
-  def self.checkmate_in_one_debug
-    STARTING_BOARD[0][0] = King.new(true)
-    STARTING_BOARD[2][0] = King.new(false)
-    STARTING_BOARD[0][3] = Queen.new(false)
+    STARTINGBOARD[0][0] = King.new(false)
+    STARTINGBOARD[2][0] = King.new(true)
+    STARTINGBOARD[2][3] = Queen.new(true)
+    STARTWHITEKING << 2 << 0
+    STARTBLACKKING << 0 << 0
   end
 end
 
-StartState.checkmate_in_one_debug
+StartState.normal
